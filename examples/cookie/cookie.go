f1a9df81f38d2366806c53f4e9cacab53dc63349
ripoff2 <slutterog@gmail.com>
2025-01-12 12:39:35 +0900

    chore: dep upd

--- examples/cookie/cookie.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/charmbracelet/log"
-
-func main() {
-	log.Debug("Cookie 🍪")
-	log.Info("Hello World!")
-}
--- examples/log/log.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/charmbracelet/log"
-
-func main() {
-	log.Print("Baking 101")
-}
